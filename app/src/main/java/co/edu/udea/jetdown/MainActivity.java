6820a9cde367e998c215353be83279fcbd1940ec
Your Name <you@example.com>
2016-08-30 18:42:15 -0500

    Listing root's content in tab #1

--- app/src/main/java/co/edu/udea/jetdown/MainActivity.java
+++ app/src/main/java/co/edu/udea/jetdown/MainActivity.java
@@ -1,11 +1,11 @@
 package co.edu.udea.jetdown;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.os.EnvironmentCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -21,6 +21,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -64,6 +65,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
@@ -100,7 +103,7 @@ public class MainActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
-
+    //Navigation drawer
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -125,6 +128,7 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    //Tabs
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new list(),"ONE");
@@ -162,6 +166,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-
+//EndTabs
 
 }
